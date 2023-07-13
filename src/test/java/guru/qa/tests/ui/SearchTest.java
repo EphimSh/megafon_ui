b205a27241822357f1237c45cb524c33fd234877
EphimSh <ephimizm@gmail.com>
2023-07-13 12:59:04 +0300

    update SearchTest.java

--- src/test/java/guru/qa/tests/ui/SearchTest.java
+++ src/test/java/guru/qa/tests/ui/SearchTest.java
@@ -1,7 +1,9 @@
 package guru.qa.tests.ui;
 
 import guru.qa.TestBase;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -22,8 +24,14 @@ public class SearchTest extends TestBase {
 
 
     @ValueSource(strings = {"интернет", "sim-карта", "usb-модем"})
-    @Tag("web")
     @ParameterizedTest(name="Результат поиска содержит текст: {0}")
+    @Tag("web")
+    @Feature("Поисковая строка на главной странице")
+    @Story("Поиск интересующих услуг и товаров")
+    @Owner("EphimSh")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(value = "spb.megafon.ru", url = "https://spb.megafon.ru")
+    @DisplayName("Ввод текста в поисковую строку")
     void searchTest(String searchItem) {
 
         step("Нажать на кнопку поиск", () -> {

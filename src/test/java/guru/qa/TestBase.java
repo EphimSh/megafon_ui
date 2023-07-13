fc030db638a699c1d9bc81c9958d1cb120187311
EphimSh <ephimizm@gmail.com>
2023-07-13 11:47:07 +0300

    update

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
 
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+//        Configuration.browserVersion = System.getProperty("browserVersion", "99.0");
         Configuration.browserSize = System.getProperty("browserSize", "1980x1080");
 //        Configuration.baseUrl = System.getProperty("baseUrl", "https://spb.megafon.ru");
 
